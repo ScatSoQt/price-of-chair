6dc2c711b447cf7e82d9a160e371a07cd06bbfdf
ScatSoQt <39046108+ScatSoQt@users.noreply.github.com>
2019-01-05 19:46:19 +0100

    with db name

--- src/common/database.py
+++ src/common/database.py
@@ -11,7 +11,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_database()
+        Database.DATABASE = client.get_database('heroku_kz0bglb8')
     @staticmethod
     def insert(collection, data):
         Database.DATABASE[collection].insert(data)
